ec9895742302ed68dcb09f15e9e6beb5ec1f5048
maxence <maxence.rouget@gmail.com>
2018-10-17 09:47:29 +0200

    Menu fait

---
+++ src/Liste.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Liste.h"
+
+using namespace std;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
-#include ".h"
+#include "Liste.h"
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+  float x,y;
+  int choose;
 
-  
+  vector <liste> tableau;
 
+  debut:
+  cout <<" --- Bonjour --- "<<endl;
+  cout <<"--- Que voulez vous faire ? ---"<<endl;
+  cout <<"--- 1__Ajouter un reel__ ---"<<endl;
+  cout <<"--- 2__Afficher la liste des reel__ ---"<<endl;
+  cout <<"--- 3__Suprimer un reel chosi__ ---"<<endl;
+  cout <<"--- 4__Suprimer tout les reel choisi__ ---"<<endl;
+  cout <<"--- 5 Quitter ---"<<endl;
+
+
+
+  fin:
+
+  return 0;
 }
