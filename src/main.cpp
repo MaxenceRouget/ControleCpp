5a6edf1cf529615bf613611f9a01e129325d216b
maxence <maxence.rouget@gmail.com>
2018-10-17 09:58:51 +0200

    Question fini

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main()
 {
-  float x,y;
-  int choose;
+  float entier,y;
+  int choose, choose2;
 
   vector <liste> tableau;
 
@@ -20,7 +20,44 @@ int main()
   cout <<"--- 4__Suprimer tout les reel choisi__ ---"<<endl;
   cout <<"--- 5 Quitter ---"<<endl;
 
+  cin>> choose;
 
+  switch(choose)
+  {
+    case 1:{
+      menu1:
+        cout <<"--- Que voulez vous faire ? ---"<<endl;
+        cout <<"1 --- Ajouter un nombre --- "<<endl;
+        cout <<"2 --- retourné au menu --- "<<endl;
+        cin>> choose2;
+
+        if(choose2 == 1)
+        {
+            cout <<"--- Saisir un entier --- "<<endl;
+              cin >> entier;
+              tableau[y].nombre = entier;
+              y++;
+              goto menu1;
+        }
+        else
+        {
+          goto debut;
+        }
+    break;}
+
+    case 2:{
+
+    break;}
+
+
+
+
+
+
+
+
+
+  }
 
   fin:
 
